ee7b2492b9d984ba67c057084f11e19dd224587b
Zhang, Wenxin <stanleyzhangga@gmail.com>
2020-03-07 19:00:20 -0500

    added bubble sort

---
+++ Bubble_Sort.py
@@ -0,0 +1,55 @@
+'''
+
+Self-developed bubble sort algorithm
+
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+
+'''
+
+import random
+
+# function definition
+def bubble_sort(nums):
+
+    # null case
+    if not nums:
+        return None
+
+    for i in range(len(nums) - 1):
+        flag = True
+        for j in range(len(nums) - i - 1):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+                flag = False
+        if flag:
+            return nums
+
+    return nums
+
+
+def check_equal(nums1, nums2):
+    if nums1 and nums2:
+        if len(nums1) != len(nums2):
+            return False
+        for i in range(len(nums1)):
+            if nums1[i] != nums2[i]:
+                return False
+        return True
+    else:
+        return nums1 == nums2
+
+
+# main test run
+if __name__ == "__main__":
+    # generate an array of random numbers
+    test_input = []
+    for _ in range(1000):
+        test_input.append(random.randint(-10, 10))
+    input_copy = test_input.copy()
+
+    test_result = bubble_sort(test_input)
+    check_result = sorted(input_copy)
+
+    print(check_equal(test_result, check_result))
+
